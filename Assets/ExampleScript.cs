e72b9a76b358006781f1191cbee45628e8f2b342
Ryan Bie <ryanbie@Ryans-MacBook-Air.local>
2017-08-26 10:52:24 -0400

    Upload Files

---
+++ Assets/ExampleScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExampleScript : MonoBehaviour {
+
+    public void Awake()
+    {
+        Debug.Log("Testing Awake");
+    }
+    public void OnEnable()
+    {
+        Debug.Log("Testing Enable");   
+    }
+    public void Start()
+    {
+        Debug.Log("Testing Start");
+    }
+    public void OnDisable()
+    {
+        Debug.Log("Testing Disable");
+    }
+}
